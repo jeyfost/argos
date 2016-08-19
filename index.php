99fb198195e4080640e100f3b79ed6353a9873ef
jeyfost <foster_andrew@tut.by>
2016-08-19 10:53:08 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name='keywords' content='мебельная фурнитура, комплектующие для мебели, Аргос-ФМ, комплектующие для мебели Могилев, Могилев, кромочные материалы, кромка, кромка ПВХ, ручки мебельные, мебельная фурнитура Могилев, кромка ПВХ Могилев, лента кромочная, лента кромочная Могилев, ручки мебельные Могилев, кромка Могилев'>
+    <meta name='description' content='Комплексные поставки всех видов мебельной фурнитуры импортного и отечественного производства. Республика Беларусь, г. Могилёв.'>
+
+    <title>Аргос-ФМ | Мебельная фурнитура</title>
+
+    <link rel='shortcut icon' href='img/icons/favicon.ico' type='image/x-icon'>
+	<link rel='icon' href='img/icons/favicon.ico' type='image/x-icon'>
+    <link rel='stylesheet' media='screen' type='text/css' href='css/style.css'>
+
+    <style>
+        #page-preloader {position: fixed; left: 0; top: 0; right: 0; bottom: 0; background: #fff; z-index: 100500;}
+        #page-preloader .spinner {width: 32px; height: 32px; position: absolute; left: 50%; top: 50%; background: url('img/system/spinner.gif') no-repeat 50% 50%; margin: -16px 0 0 -16px;}
+    </style>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+
+    <script type="text/javascript">
+        $(window).on('load', function () {
+            var $preloader = $('#page-preloader'), $spinner = $preloader.find('.spinner');
+            $spinner.delay(500).fadeOut();
+            $preloader.delay(850).fadeOut('slow');
+        });
+    </script>
+
+</head>
+
+<body>
+
+    <div id="page-preloader"><span class="spinner"></span></div>
+
+    <div id="menu">
+        <div class="container" style="height: 100%;">
+            <a href="index.php"><img src="img/system/logo.png" id="logo" /></a>
+            <div id="menuLinks">
+            1/ 2/ 3/ 4/ 5/ 6
+        </div>
+        <div style="clear: both;"></div>
+        </div>
+
+    </div>
+    <div id="menuShadow"></div>
+
+    <div id="mainImg"><img src="img/system/main.JPG" width="100%" /></div>
+
+</body>
+
+</html>
