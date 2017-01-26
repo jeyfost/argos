8675a343107255f76b07c46821782577d74ec106
jeyfost <foster_andrew@tut.by>
2017-01-26 15:39:57 +0300

    Добавлена страница фирменной сети

--- js/menu1.js
+++ js/menu1.js
@@ -3,25 +3,25 @@ function showDropdownList(action, id, highlighted) {
         switch(id) {
             case "catalogueLink":
                 if(highlighted == "catalogueLinkFA") {
-                    $('#dropDownList').html("<div class='dropDownLink' id='catalogueLinkFA' style='background-color: #df4e47;'><a href='../catalogue.php?type=fa&p=1' id='catalogueLinkFAA' style='color: #fff'>Мебельная фурнитура</a></div>");
+                    $('#dropDownList').html("<div class='dropDownLink' style='background-color: #df4e47;'><a href='../catalogue.php?type=fa&p=1' id='catalogueLinkFAA' style='color: #fff'>Мебельная фурнитура</a></div>");
                 } else {
 					$('#dropDownList').html("<div class='dropDownLink' id='catalogueLinkFA' onmouseover='changeDropDownLink(\"1\", \"catalogueLinkFA\", \"catalogueLinkFAA\")' onmouseout='changeDropDownLink(\"0\", \"catalogueLinkFA\", \"catalogueLinkFAA\")'><a href='../catalogue.php?type=fa&p=1' id='catalogueLinkFAA'>Мебельная фурнитура</a></div>");
 				}
 
 				if(highlighted == "catalogueLinkEM") {
-					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='catalogueLinkEM' style='background-color: #df4e47;'><a href='../catalogue.php?type=em&p=1' id='catalogueLinkEMA' style='color: #fff'>Кромочные материалы</a></div>");
+					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' style='background-color: #df4e47;'><a href='../catalogue.php?type=em&p=1' id='catalogueLinkEMA' style='color: #fff'>Кромочные материалы</a></div>");
 				} else {
 					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='catalogueLinkEM' onmouseover='changeDropDownLink(\"1\", \"catalogueLinkEM\", \"catalogueLinkEMA\")' onmouseout='changeDropDownLink(\"0\", \"catalogueLinkEM\", \"catalogueLinkEMA\")'><a href='../catalogue.php?type=em&p=1' id='catalogueLinkEMA'>Кромочные материалы</a></div>");
 				}
 
 				if(highlighted == "catalogueLinkCA") {
-					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='catalogueLinkCA' style='background-color: #df4e47;'><a href='../catalogue.php?type=ca&p=1' id='catalogueLinkCAA' style='color: #fff'>Аксессуары для штор</a></div>");
+					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' style='background-color: #df4e47;'><a href='../catalogue.php?type=ca&p=1' id='catalogueLinkCAA' style='color: #fff'>Аксессуары для штор</a></div>");
 				} else {
 					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='catalogueLinkCA' onmouseover='changeDropDownLink(\"1\", \"catalogueLinkCA\", \"catalogueLinkCAA\")' onmouseout='changeDropDownLink(\"0\", \"catalogueLinkCA\", \"catalogueLinkCAA\")'><a href='../catalogue.php?type=ca&p=1' id='catalogueLinkCAA'>Аксессуары для штор</a></div>");
 				}
 
 				if(highlighted == "catalogueLinkDG") {
-					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='catalogueLinkDG' style='background-color: #df4e47;'><a href='../catalogue.php?type=dg&p=1' id='catalogueLinkDGA' style='color: #fff'>Сопутствующие товары</a></div>");
+					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' style='background-color: #df4e47;'><a href='../catalogue.php?type=dg&p=1' id='catalogueLinkDGA' style='color: #fff'>Сопутствующие товары</a></div>");
 				} else {
 					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='catalogueLinkDG' onmouseover='changeDropDownLink(\"1\", \"catalogueLinkDG\", \"catalogueLinkDGA\")' onmouseout='changeDropDownLink(\"0\", \"catalogueLinkDG\", \"catalogueLinkDGA\")'><a href='../catalogue.php?type=dg&p=1' id='catalogueLinkDGA'>Сопутствующие товары</a></div>");
 				}
@@ -30,56 +30,66 @@ function showDropdownList(action, id, highlighted) {
                 break;
             case "aboutLink":
 				if(highlighted == "aboutLinkCommon") {
-					$('#dropDownList').html("<div class='dropDownLink' id='aboutLinkCommon' style='background-color: #df4e47;'><a href='../about/info.php' id='aboutLinkCommonA' style='color: #fff;'>Общая информация</a></div>");
+					$('#dropDownList').html("<div class='dropDownLink' style='background-color: #df4e47;'><a href='../about/info.php' id='aboutLinkCommonA' style='color: #fff;'>Общая информация</a></div>");
 				} else {
 					$('#dropDownList').html("<div class='dropDownLink' id='aboutLinkCommon' onmouseover='changeDropDownLink(\"1\", \"aboutLinkCommon\", \"aboutLinkCommonA\")' onmouseout='changeDropDownLink(\"0\", \"aboutLinkCommon\", \"aboutLinkCommonA\")'><a href='../about/info.php' id='aboutLinkCommonA'>Общая информация</a></div>");
 				}
 
 				if(highlighted == "aboutLinkAssortment") {
-					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='aboutLinkAssortment' style='background-color: #df4e47;'><a href='../about/assortment.php' id='aboutLinkAssortmentA' style='color: #fff;'>Ассортимент</a></div>");
+					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' style='background-color: #df4e47;'><a href='../about/assortment.php' id='aboutLinkAssortmentA' style='color: #fff;'>Ассортимент</a></div>");
 				} else {
 					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='aboutLinkAssortment' onmouseover='changeDropDownLink(\"1\", \"aboutLinkAssortment\", \"aboutLinkAssortmentA\")' onmouseout='changeDropDownLink(\"0\", \"aboutLinkAssortment\", \"aboutLinkAssortmentA\")'><a href='../about/assortment.php' id='aboutLinkAssortmentA'>Ассортимент</a></div>");
 				}
 
 				if(highlighted == "aboutLinkAwards") {
-					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='aboutLinkAwards' style='background-color: #df4e47;'><a href='../about/awards.php' id='aboutLinkAwardsA' style='color: #fff;'>Достижения и награды</a></div>");
+					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' style='background-color: #df4e47;'><a href='../about/awards.php' id='aboutLinkAwardsA' style='color: #fff;'>Достижения и награды</a></div>");
 				} else {
 					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='aboutLinkAwards' onmouseover='changeDropDownLink(\"1\", \"aboutLinkAwards\", \"aboutLinkAwardsA\")' onmouseout='changeDropDownLink(\"0\", \"aboutLinkAwards\", \"aboutLinkAwardsA\")'><a href='../about/awards.php' id='aboutLinkAwardsA'>Достижения и награды</a></div>");
 				}
 
 				if(highlighted == "aboutLinkGallery") {
-					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='aboutLinkGallery' style='background-color: #df4e47;'><a href='../about/gallery.php' id='aboutLinkGalleryA' style='color: #fff;'>Фотогаллерея</a></div>");
+					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' style='background-color: #df4e47;'><a href='../about/gallery.php' id='aboutLinkGalleryA' style='color: #fff;'>Фотогаллерея</a></div>");
 				} else {
 					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='aboutLinkGallery' onmouseover='changeDropDownLink(\"1\", \"aboutLinkGallery\", \"aboutLinkGalleryA\")' onmouseout='changeDropDownLink(\"0\", \"aboutLinkGallery\", \"aboutLinkGalleryA\")'><a href='../about/gallery.php' id='aboutLinkGalleryA'>Фотогаллерея</a></div>");
 				}
 
 				if(highlighted == "aboutLinkVacancies") {
-					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='aboutLinkVacancies' style='background-color: #df4e47;'><a href='../about/vacancies.php' id='aboutLinkVacanciesA' style='color: #fff;'>Вакансии</a></div><div style='clear: both;'></div>");
+					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' style='background-color: #df4e47;'><a href='../about/vacancies.php' id='aboutLinkVacanciesA' style='color: #fff;'>Вакансии</a></div><div style='clear: both;'></div>");
 				} else {
 					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='aboutLinkVacancies' onmouseover='changeDropDownLink(\"1\", \"aboutLinkVacancies\", \"aboutLinkVacanciesA\")' onmouseout='changeDropDownLink(\"0\", \"aboutLinkVacancies\", \"aboutLinkVacanciesA\")'><a href='../about/vacancies.php' id='aboutLinkVacanciesA'>Вакансии</a></div><div style='clear: both;'></div>");
 				}
                 break;
             case "storesLink":
-                $('#dropDownList').html("<div class='dropDownLink' id='storesLinkCompany' onmouseover='changeDropDownLink(\"1\", \"storesLinkCompany\", \"storesLinkCompanyA\")' onmouseout='changeDropDownLink(\"0\", \"storesLinkCompany\", \"storesLinkCompanyA\")'><a href='../stores/company.php' id='storesLinkCompanyA'>Фирменная сеть</a></div><div class='dropDownLink' id='storesLinkRepresentatives' onmouseover='changeDropDownLink(\"1\", \"storesLinkRepresentatives\", \"storesLinkRepresentativesA\")' onmouseout='changeDropDownLink(\"0\", \"storesLinkRepresentatives\", \"storesLinkRepresentativesA\")'><a href='../stores/representatives.php' id='storesLinkRepresentativesA'>Партнёрская сеть</a></div><div style='clear: both;'></div>");
+				if(highlighted == "storesLinkCompany") {
+					$('#dropDownList').html("<div class='dropDownLink' style='background-color: #df4e47;'><a href='../stores/company.php' id='storesLinkCompanyA' style='color: #fff;'>Фирменная сеть</a></div>");
+				} else {
+					$('#dropDownList').html("<div class='dropDownLink' id='storesLinkCompany' onmouseover='changeDropDownLink(\"1\", \"storesLinkCompany\", \"storesLinkCompanyA\")' onmouseout='changeDropDownLink(\"0\", \"storesLinkCompany\", \"storesLinkCompanyA\")'><a href='../stores/company.php' id='storesLinkCompanyA'>Фирменная сеть</a></div>");
+				}
+
+				if(highlighted == "storesLinkRepresentatives") {
+					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' style='background-color: #df4e47;'><a href='../stores/representatives.php' id='storesLinkRepresentativesA' style='color: #fff;'>Партнёрская сеть</a></div><div style='clear: both;'></div>");
+				} else {
+					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='storesLinkRepresentatives' onmouseover='changeDropDownLink(\"1\", \"storesLinkRepresentatives\", \"storesLinkRepresentativesA\")' onmouseout='changeDropDownLink(\"0\", \"storesLinkRepresentatives\", \"storesLinkRepresentativesA\")'><a href='../stores/representatives.php' id='storesLinkRepresentativesA'>Партнёрская сеть</a></div><div style='clear: both;'></div>");
+				}
                 break;
             case "partnersLink":
                 $('#dropDownList').html("<div class='dropDownLink' id='partnersLinkCooperation' onmouseover='changeDropDownLink(\"1\", \"partnersLinkCooperation\", \"partnersLinkCooperationA\")' onmouseout='changeDropDownLink(\"0\", \"partnersLinkCooperation\", \"partnersLinkCooperationA\")'><a href='../partners/cooperation.php' id='partnersLinkCooperationA'>Сотрудничество</a></div><div class='dropDownLink' id='partnersLinkClientNews' onmouseover='changeDropDownLink(\"1\", \"partnersLinkClientNews\", \"partnersLinkClientNewsA\")' onmouseout='changeDropDownLink(\"0\", \"partnersLinkClientNews\", \"partnersLinkClientNewsA\")'><a href='../partners/news.php' id='partnersLinkClientNewsA'>Новости для клиентов</a></div><div style='clear: both;'></div>");
                 break;
             case "contactsLink":
                 if(highlighted == "contactsLinkStores") {
-					$('#dropDownList').html("<div class='dropDownLink' id='contactsLinkStores' style='background-color: #df4e47;'><a href='../contacts/stores.php' id='contactsLinkStoresA' style='color: #fff;'>Магазины</a></div>")
+					$('#dropDownList').html("<div class='dropDownLink' style='background-color: #df4e47;'><a href='../contacts/stores.php' id='contactsLinkStoresA' style='color: #fff;'>Магазины</a></div>")
 				} else {
 					$('#dropDownList').html("<div class='dropDownLink' id='contactsLinkStores' onmouseover='changeDropDownLink(\"1\", \"contactsLinkStores\", \"contactsLinkStoresA\")' onmouseout='changeDropDownLink(\"0\", \"contactsLinkStores\", \"contactsLinkStoresA\")'><a href='../contacts/stores.php' id='contactsLinkStoresA'>Магазины</a></div>");
 				}
 
 				if(highlighted == "contactsLinkMain") {
-					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='contactsLinkMain' style='background-color: #df4e47;'><a href='../contacts/main.php' id='contactsLinkMainA' style='color: #fff;'>Головное предприятие</a></div>");
+					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' style='background-color: #df4e47;'><a href='../contacts/main.php' id='contactsLinkMainA' style='color: #fff;'>Головное предприятие</a></div>");
 				} else {
 					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' id='contactsLinkMain' onmouseover='changeDropDownLink(\"1\", \"contactsLinkMain\", \"contactsLinkMainA\")' onmouseout='changeDropDownLink(\"0\", \"contactsLinkMain\", \"contactsLinkMainA\")'><a href='../contacts/main.php' id='contactsLinkMainA'>Головное предприятие</a></div>");
 				}
 
 				if(highlighted == "contactsLinkMail") {
-					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink'  id='contactsLinkMail' style='background-color: #df4e47;'><a href='../contacts/mail.php' id='contactsLinkMailA' style='color: #fff;'>Обратная связь</a></div><div style='clear: both;'></div>");
+					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink' style='background-color: #df4e47;'><a href='../contacts/mail.php' id='contactsLinkMailA' style='color: #fff;'>Обратная связь</a></div><div style='clear: both;'></div>");
 				} else {
 					$('#dropDownList').html($('#dropDownList').html() + "<div class='dropDownLink'  id='contactsLinkMail' onmouseover='changeDropDownLink(\"1\", \"contactsLinkMail\", \"contactsLinkMailA\")' onmouseout='changeDropDownLink(\"0\", \"contactsLinkMail\", \"contactsLinkMailA\")'><a href='../contacts/mail.php' id='contactsLinkMailA'>Обратная связь</a></div><div style='clear: both;'></div>");
 				}
---
+++ stores/company.php
@@ -0,0 +1,254 @@
+<?php
+
+session_start();
+include("../scripts/connect.php");
+
+if(isset($_SESSION['userID'])) {
+	if(isset($_COOKIE['argosfm_login']) and isset($_COOKIE['argosfm_password'])) {
+		setcookie("argosfm_login", "", 0, '/');
+		setcookie("argosfm_password", "", 0, '/');
+		setcookie("argosfm_login", $_COOKIE['argosfm_login'], time()+60*60*24*30*12, '/');
+		setcookie("argosfm_password", $_COOKIE['argosfm_password'], time()+60*60*24*30*12, '/');
+	}
+	else {
+		$userResult = $mysqli->query("SELECT * FROM users WHERE id = '".$_SESSION['userID']."'");
+		$user = $userResult->fetch_assoc();
+		setcookie("argosfm_login", $user['login'], time()+60*60*24*30*12, '/');
+		setcookie("argosfm_password", $user['password'], time()+60*60*24*30*12, '/');
+	}
+}
+
+if(isset($_SESSION['userID'])) {
+	$loginsCountResult = $mysqli->query("SELECT logins_count FROM users WHERE id = '".$_SESSION['userID']."'");
+	$loginsCount = $loginsCountResult->fetch_array(MYSQLI_NUM);
+	$count = $loginsCount[0] + 1;
+
+	$mysqli->query("UPDATE users SET last_login = '".date('d-m-Y H:i:s')."', logins_count = '".$count."' WHERE id = '".$_SESSION['userID']."'");
+}
+
+?>
+
+<!doctype html>
+
+<html>
+
+<head>
+ 	<meta charset="utf-8">
+
+    <title>Вакансии</title>
+
+    <link rel='shortcut icon' href='../img/icons/favicon.ico' type='image/x-icon'>
+    <link rel='stylesheet' media='screen' type='text/css' href='../css/style.css'>
+	<link rel="stylesheet" type="text/css" href="../js/lightview/css/lightview/lightview.css" />
+    <?php
+		if(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== false) {
+			echo "<link rel='stylesheet' media='screen' type='text/css' href='../css/styleOpera.css'>";
+		}
+	?>
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="../js/menu1.js"></script>
+	<script type="text/javascript" src="../js/common.js"></script>
+	<!--[if lt IE 9]>
+  		<script type="text/javascript" src="../js/lightview/js/excanvas/excanvas.js"></script>
+	<![endif]-->
+	<script type="text/javascript" src="../js/lightview/js/spinners/spinners.min.js"></script>
+	<script type="text/javascript" src="../js/lightview/js/lightview/lightview.js"></script>
+
+	<style>
+		#page-preloader {position: fixed; left: 0; top: 0; right: 0; bottom: 0; background: #fff; z-index: 100500;}
+		#page-preloader .spinner {width: 32px; height: 32px; position: absolute; left: 50%; top: 50%; background: url('../img/system/spinner.gif') no-repeat 50% 50%; margin: -16px 0 0 -16px;}
+	</style>
+
+	<script type="text/javascript">
+        $(window).on('load', function () {
+            var $preloader = $('#page-preloader'), $spinner = $preloader.find('.spinner');
+            $spinner.delay(500).fadeOut();
+            $preloader.delay(850).fadeOut('slow');
+        });
+    </script>
+</head>
+
+<body>
+	<div id="page-preloader"><span class="spinner"></span></div>
+
+    <div id="menu">
+        <div class="container" style="height: 100%;">
+            <a href="../index.php"><img src="../img/system/logo.png" id="logo" /></a>
+			<div id="personalButtons">
+				<?php
+					if(isset($_SESSION['userID'])) {
+						echo "
+							<div class='headerIcon'>
+								<a href='../scripts/personal/logout.php'><img src='../img/system/exit.png' title='Выйти из своей учётной записи' id='exitIMG' onmouseover='changeIcon(\"exitIMG\", \"exitRed.png\", 1)' onmouseout='changeIcon(\"exitIMG\", \"exit.png\", 1)' /></a>
+							</div>
+							<div class='headerIcon'>
+								<a href='../personal/personal.php?section=1'><img src='../img/system/personal.png' title='Личный кабинет' id='personalIMG' onmouseover='changeIcon(\"personalIMG\", \"personalRed.png\", 1)' onmouseout='changeIcon(\"personalIMG\", \"personal.png\", 1)' /></a>
+							</div>
+						";
+						if($_SESSION['userID'] == 1) {
+							echo "
+								<div class='headerIcon'>
+									<a href='../personal/orders.php'><img src='../img/system/basketFull.png' title='Заявки' id='basketIMG' onmouseover='changeIcon(\"basketIMG\", \"basketFullRed.png\", 1)' onmouseout='changeIcon(\"basketIMG\", \"basketFull.png\", 1)' /></a>
+								</div>
+							";
+						} else {
+							$basketQuantityResult = $mysqli->query("SELECT COUNT(id) FROM basket WHERE user_id = '".$_SESSION['userID']."'");
+							$basketQuantity = $basketQuantityResult->fetch_array(MYSQLI_NUM);
+
+							if($basketQuantity[0] > 0) {
+								echo "
+									<div class='headerIcon' id='basketIcon'>
+										<a href='../personal/basket.php?section=1' onmouseover='changeIcon(\"basketIMG\", \"basketFullRed.png\", 1)' onmouseout='changeIcon(\"basketIMG\", \"basketFull.png\", 1)'><img src='../img/system/basketFull.png' title='Корзина | Товаров в корзине: ".$basketQuantity[0]."' id='basketIMG' /><div id='basketLabel'>".$basketQuantity[0]."</div></a>
+									</div>
+								";
+							} else {
+								echo "
+									<div class='headerIcon' id='basketIcon'>
+										<a href='../personal/basket.php'><img src='../img/system/basketFull.png' title='Корзина | Товаров в корзине: ".$basketQuantity[0]."' id='basketIMG' onmouseover='changeIcon(\"basketIMG\", \"basketFullRed.png\", 1)' onmouseout='changeIcon(\"basketIMG\", \"basketFull.png\", 1)' /></a>
+									</div>
+								";
+							}
+						}
+					} else {
+						echo "
+							<div class='headerIcon'>
+								<a href='../personal/login.php'><img src='../img/system/login.png' title='Войти под своей учётной записью' id='loginIMG' onmouseover='changeIcon(\"loginIMG\", \"loginRed.png\", 1)' onmouseout='changeIcon(\"loginIMG\", \"login.png\", 1)' /></a>
+							</div>
+						";
+					}
+					echo "
+						<div id='searchBlock'>
+							<form method='post'>
+								<input type='text' id='searchFieldInput' name=searchField' value='Поиск...' />
+							</form>
+						</div>
+					";
+					echo "<div style='clear: both;'></div>";
+				?>
+			</div>
+			<div id="menuLinks">
+				<div class="menuLink" id="catalogueLink" onmouseover="showDropdownList('1', 'catalogueLink')">
+					<a href="../catalogue.php?type=fa&p=1">Каталог</a>
+					<img src="../img/system/downArrow.png" />
+					<span class="slash"> /</span>
+				</div>
+				<div class="menuLink" id="aboutLink" onmouseover="showDropdownList('1', 'aboutLink')">
+					<a href="../about/info.php">О компании</a>
+					<img src="../img/system/downArrow.png" />
+					<span class="slash"> /</span>
+				</div>
+				<div class="menuLinkNotDD">
+					<a href="../news.php">Новости</a>
+					<span class="slash"> /</span>
+				</div>
+				<div class="menuLink" id="storesLink" onmouseover="showDropdownList('1', 'storesLink', 'storesLinkCompany')">
+					<a href="../stores/company.php" style="color: #df4e47;">Где купить</a>
+					<img src="../img/system/downArrow.png" />
+					<span class="slash"> /</span>
+				</div>
+				<div class="menuLinkNotDD">
+					<a href="../actions.php">Акции</a>
+					<span class="slash"> /</span>
+				</div>
+				<div class="menuLink" id="partnersLink" onmouseover="showDropdownList('1', 'partnersLink')">
+					<a href="../partners/cooperation.php">Партнерам</a>
+					<img src="../img/system/downArrow.png" />
+					<span class="slash"> /</span>
+				</div>
+				<div class="menuLink" id="contactsLink" onmouseover="showDropdownList('1', 'contactsLink')">
+					<a href="../contacts/stores.php">Контакты</a>
+					<img src="../img/system/downArrow.png" />
+				</div>
+				<div style="clear: both;"></div>
+			</div>
+			<div style="clear: both;"></div>
+        </div>
+
+    </div>
+    <div id="dropDownLine">
+        <div id="dropDownArrowContainer">
+            <img src="../img/system/dropDownListArrow.png" id="dropDownArrow" />
+        </div>
+        <div id="dropDownList"></div>
+    </div>
+    <div id="menuShadow"></div>
+
+	<div id="page">
+		<div id="searchList"></div>
+		<h1 style='margin-top: 80px;'>Фирменная сеть магазинов</h1>
+		<div id='breadCrumbs'>
+			<a href='../index.php'><span class='breadCrumbsText'>Главная</span></a> > <a href='company.php'><span class='breadCrumbsText'>Где купить</span></a> > <a href='company.php'><span class='breadCrumbsText'>Фирменная сеть</span></a>
+		</div>
+
+		<div id="personalMenu">
+			<a href='company.php'><div class='personalMenuLinkActive'>Фирменная сеть</div></a>
+			<div style='width: 100%; height: 5px;'></div>
+			<a href='representatives.php'><div class='personalMenuLink' id='pbl2' onmouseover='buttonChange("pbl2", 1)' onmouseout='buttonChange("pbl2", 0)'>Партнёрская сеть</div></a>
+		</div>
+
+		<div id="personalContent">
+			<div class="mapContainer">
+				<div class="map">
+					<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=om0QHqcbbq7ki3IzQujwQF5MaychC7ZG&amp;width=500&amp;height=400&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=true"></script>
+				</div>
+				<div class="mapDescription">
+					<h2>1. Магазин мебельной фурнитуры</h2>
+					<b>Адрес:</b>
+					<br />
+					<span>212011, Республика Беларусь, г. Могилёв, ул. Калужская, 13</span>
+					<br /><br />
+					<b>Телефон:</b>
+					<br />
+					<span>+375 (29) xxx-xx-xx</span>
+					<br /><br />
+					<b>Время работы:</b>
+					<br />
+					<span>понедельник - пятница</span>
+					<br />
+					<span>9:00 - 18:00</span>
+				</div>
+				<div style="clear: both;"></div>
+			</div>
+			<br /><br />
+			<div class="mapContainer">
+				<div class="map">
+					<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=TRFDdk4LfiXmytO-3UCq_hXaFXJG-3Zd&amp;width=500&amp;height=400&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=true"></script>
+				</div>
+				<div class="mapDescription">
+					<h2>2. Магазин мебельной фурнитуры</h2>
+					<b>Адрес:</b>
+					<br />
+					<span>212040, Республика Беларусь, г. Могилёв, ул. Залуцкого, 21</span>
+					<br /><br />
+					<b>Телефон:</b>
+					<br />
+					<span>+375 (29) xxx-xx-xx</span>
+					<br /><br />
+					<b>Время работы:</b>
+					<br />
+					<span>понедельник - пятница</span>
+					<br />
+					<span>8:00 - 17:00</span>
+				</div>
+				<div style="clear: both;"></div>
+			</div>
+		</div>
+	</div>
+
+	<div style="clear: both;"></div>
+	<div id="space"></div>
+
+    <div id="footerShadow"></div>
+    <div id="footer">
+		<div class="container">
+			<div class="copy">&copy; ЧТУП &laquo;Аргос-ФМ&raquo;<br />2008 - <?php echo date('Y'); ?></div>
+			<div class="copy" style="margin-left: 40px;">Республика Беларусь, г. Могилёв, ул. Залуцкого, 21<br /><a href="contacts.php?page=main">Контактная информация</a></div>
+			<div class="copy" style="float: right;">Разработка сайта<br /><a href="http://airlab.by/">airlab</a></div>
+		</div>
+		<div style="clear: both;"></div>
+	</div>
+
+</body>
+
+</html>
