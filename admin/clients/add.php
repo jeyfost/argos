a9ddab1f6175b4347bd23044ea1bf2ab76d3bc54
jeyfost <foster_andrew@tut.by>
2017-04-05 10:59:10 +0300

    Добавлена стартовая страница клиентской базы

---
+++ admin/clients/add.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeyfost
+ * Date: 05.04.2017
+ * Time: 9:53
+ */
+
--- admin/clients/index.php
+++ admin/clients/index.php
@@ -169,7 +169,7 @@ if(isset($_SESSION['userID'])) {
 			<br />
 			<h2>&darr; Для продолжения работы выберите раздел</h2>
 			<a href="database.php"><input type="button" class="button" id="databaseButton" value="Клиентская база" onmouseover="buttonChange('databaseButton', 1)" onmouseout="buttonChange('databaseButton', 0)" style="margin-left: 0;" /></a>
-			<a href="add.php"><input type="button" class="button" id="addButton" value="Добавление" onmouseover="buttonChange('historyButton', 1)" onmouseout="buttonChange('historyButton', 0)" /></a>
+			<a href="add.php"><input type="button" class="button" id="addButton" value="Добавление" onmouseover="buttonChange('addButton', 1)" onmouseout="buttonChange('addButton', 0)" /></a>
 			<div style="clear: both;"></div>
 		</div>
 		<div style="clear: both;"></div>
