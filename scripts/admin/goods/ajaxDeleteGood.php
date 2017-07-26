b31941dc1a2b1b0debd7016639810d4a4857ad14
jeyfost <foster_andrew@tut.by>
2017-07-26 16:26:39 +0300

    Реализовано удаление дополнительных фотографий при удалении товара

--- scripts/admin/goods/ajaxDeleteGood.php
+++ scripts/admin/goods/ajaxDeleteGood.php
@@ -15,6 +15,14 @@ if($mysqli->query("DELETE FROM catalogue_new WHERE id = '".$goodID."'")) {
 	unlink("../../../img/catalogue/big/".$good['picture']);
 	unlink("../../../img/catalogue/small/".$good['small']);
 
+	$photoResult = $mysqli->query("SELECT * FROM goods_photos WHERE good_id = '".$goodID."'");
+	while($photo = $photoResult->fetch_assoc()) {
+		unlink("../../../img/catalogue/photos/big/".$photo['big']);
+		unlink("../../../img/catalogue/photos/small/".$photo['small']);
+	}
+
+	$mysqli->query("DELETE FROM goods_photos WHERE good_id = '".$goodID."'");
+
 	echo "ok";
 } else {
 	echo "failed";
