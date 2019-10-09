877966c366ec68cdf9899af16a4ad582a2df50b1
jeyfost <foster_andrew@tut.by>
2019-10-09 16:11:28 +0300

    Исправлено добавление нового подраздела третьего уровня

--- scripts/admin/sections/ajaxCheckSubcategory.php
+++ scripts/admin/sections/ajaxCheckSubcategory.php
@@ -12,10 +12,10 @@ $goodSubcategory2 = $mysqli->real_escape_string($_POST['goodSubcategory2']);
 $sectionName = $mysqli->real_escape_string($_POST['name']);
 
 if(!empty($goodSubcategory)) {
-	$categoryCheckResult = $mysqli->query("SELECT COUNT(id) FROM subcategories2 WHERE name = '".$sectionName."' AND id <> '".$goodSubcategory2."'");
+	$categoryCheckResult = $mysqli->query("SELECT COUNT(id) FROM subcategories2 WHERE name = '".$sectionName."' AND subcategory = '".$goodSubcategory."'");
 	$categoryCheck = $categoryCheckResult->fetch_array(MYSQLI_NUM);
 } else {
-	$categoryCheckResult = $mysqli->query("SELECT COUNT(id) FROM subcategories_new WHERE name = '".$sectionName."' AND id <> '".$goodSubcategory."'");
+	$categoryCheckResult = $mysqli->query("SELECT COUNT(id) FROM subcategories_new WHERE name = '".$sectionName."' AND category = '".$goodCategory."'");
 	$categoryCheck = $categoryCheckResult->fetch_array(MYSQLI_NUM);
 }
 
