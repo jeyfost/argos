e0f65152a8adf48720d3e44a676c929e0546c0ae
jeyfost <foster_andrew@tut.by>
2019-10-14 11:46:33 +0300

    Обновлены телефонные номера в футере

--- layouts/footer.php
+++ layouts/footer.php
@@ -11,7 +11,7 @@ function footer() {
         <div class='container'>
             <div class='copy'>&copy; ЧТУП &laquo;Аргос-ФМ&raquo;<br />2008 - ".date('Y')."</div>
             <div class='copy' style='margin-left: 40px;'>Республика Беларусь, г. Могилёв, ул. Залуцкого, 21<br /><a href='/contacts/main.php'>Контактная информация</a> | <a href='/sitemap.php'>Карта сайта</a></div>
-            <div class='copy' style='float: right;'><i class='fa fa-phone' aria-hidden='true'></i> +375 (222) 747-800<br /><i class='fa fa-phone' aria-hidden='true'></i> +375 (222) 707-707</div>
+            <div class='copy' style='float: right;'><i class='fa fa-phone' aria-hidden='true'></i> ".SALES_CITY."<br /><i class='fa fa-phone' aria-hidden='true'></i> ".SALES_MOBILE."</div>
         </div>
         <div style='clear: both;'></div>
     ";
